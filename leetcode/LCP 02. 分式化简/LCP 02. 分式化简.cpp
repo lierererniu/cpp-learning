8ae3fdacf83e3a3004b9b1d85663c2d8732e24f0
XY LI <41353851+lierererniu@users.noreply.github.com>
2022-03-13 20:52:00 +0800

    Add files via upload

---
+++ leetcode/LCP 02. 分式化简/LCP 02. 分式化简.cpp
@@ -0,0 +1,35 @@
+//
+// Created by 53110 on 2022/3/13.
+//
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class SolutionLCP {
+public:
+    vector<int> fraction(vector<int> &cont) {
+        int length = cont.size();
+        vector<int> ret = {1, cont[length - 1]};
+        vector<int> ret1 = {0, 0};
+        for (int i = length - 1; i > 0; --i) {
+            ret1[0] = cont[i - 1] * ret[1];
+            ret1[1] = ret[1];
+            ret[0] = ret1[0] + ret[0];//分子
+            ret[1] = ret1[1];//分母
+            swap(ret[0], ret[1]); //翻转
+        }
+        swap(ret[0], ret[1]);
+        return ret;
+    }
+};
+
+int main() {
+    vector<int> s = {3, 2, 0, 2};
+    vector<int> res = SolutionLCP().fraction(s);
+    for (auto &it: res) {
+        cout << it << " ";
+    }
+    cout << endl;
+    return 0;
+}
