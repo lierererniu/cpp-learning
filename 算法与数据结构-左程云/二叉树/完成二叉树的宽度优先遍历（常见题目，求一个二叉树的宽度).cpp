63da499e3e4f128d2ab71cec0f7a5193f1fa9b41
XY LI <41353851+lierererniu@users.noreply.github.com>
2022-03-28 22:05:08 +0800

    Add files via upload

---
+++ 算法与数据结构-左程云/二叉树/完成二叉树的宽度优先遍历（常见题目，求一个二叉树的宽度).cpp
@@ -0,0 +1,79 @@
+//
+// Created by 53110 on 2022/3/27.
+//
+#include <queue>
+#include <iostream>
+#include <map>
+#include<limits.h>
+using namespace std;
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(): val(0), left(nullptr), right(nullptr){}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr){}
+    TreeNode(int x, TreeNode *left, TreeNode *right): val(x), left(left), right(right){}
+};
+// 队列 求最大宽度
+class PreWidthOrder{
+public:
+    // 哈希表
+    int WidthOrder(TreeNode *root){
+        if (root == nullptr){
+            return 0;
+        }
+        //
+        map<TreeNode *, int> levelMap;
+        levelMap[root] = 1;
+        // 记录当前层
+        int curLevel = 1;
+        int curLevelNodes = 0;
+        int maxW = INT_MIN;
+
+        queue<TreeNode *> que;
+        que.push(root);
+        while(!que.empty()){
+            TreeNode *cur = que.front();
+            que.pop();
+            int curNodeLevel = levelMap[cur];
+            // 层数没变，该层节点数 + 1
+            if(curNodeLevel == curLevel){
+                curLevelNodes ++;
+            }else{
+                maxW = max(maxW, curLevelNodes);
+                curLevel++;
+                curLevelNodes = 1;
+            }
+            if (cur->left != nullptr){
+                levelMap[cur->left] = curNodeLevel + 1;
+                que.push(root->left);
+            }
+            if (cur->right != nullptr){
+                levelMap[cur->right] = curNodeLevel + 1;
+                que.push(cur->right);
+            }
+        }
+        return maxW;
+    }
+    // 非哈希表方法
+    int WidthOrderNoMap(TreeNode *root){
+
+    }
+};
+
+int main() {
+    TreeNode *head = new TreeNode(5);
+    head->left = new TreeNode(3);
+    head->right = new TreeNode(8);
+    head->left->left = new TreeNode(2);
+    head->left->right = new TreeNode(4);
+    head->left->left->left = new TreeNode(1);
+    head->right->left = new TreeNode(7);
+    head->right->left->left = new TreeNode(6);
+    head->right->right = new TreeNode(10);
+    head->right->right->left = new TreeNode(9);
+    head->right->right->right = new TreeNode(11);
+    int res = PreWidthOrder().WidthOrder(head);
+    cout<<res;
+    return 0;
+}
